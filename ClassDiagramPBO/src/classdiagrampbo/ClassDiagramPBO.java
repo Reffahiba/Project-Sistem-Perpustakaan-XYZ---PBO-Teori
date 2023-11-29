254da1563bfde90f0bb2f30b8835f786eda3e6a2
Reffa Hibatullah <112398681+Reffahiba@users.noreply.github.com>
2023-11-30 01:37:44 +0700

    Update ClassDiagramPBO.java

--- ClassDiagramPBO/src/classdiagrampbo/ClassDiagramPBO.java
+++ ClassDiagramPBO/src/classdiagrampbo/ClassDiagramPBO.java
@@ -19,57 +19,44 @@ public class ClassDiagramPBO {
      */
     public static void main(String[] args) {
         Admin admin = new Admin("ID01", "Rehan");
-        AnggotaPerpustakaan ap1 = new AnggotaPerpustakaan("IDA001", "Reffa", "082178570012");
-        AnggotaPerpustakaan ap2 = new AnggotaPerpustakaan("IDA002", "Rifandi", "082198123026");
-        TransaksiPeminjaman tp1 = new TransaksiPeminjaman("TP001", new Date(123,10,28) ,new Date(123,10,29) ,new Date() , ap1);
-        TransaksiPengembalian tpengem1 = new TransaksiPengembalian(tp1);
-
+        AnggotaPerpustakaan ap1 = new AnggotaPerpustakaan("IDA001", "Reffa", "082178424252");
+        AnggotaPerpustakaan ap2 = new AnggotaPerpustakaan("IDA002", "Rifandi", "08219812302");
+        TransaksiPeminjaman tpin1 = new TransaksiPeminjaman("TP001", new Date(123,10,28) ,new Date(123,10,29) ,new Date() , ap1);
+        TransaksiPengembalian tkem1 = new TransaksiPengembalian(tpin1);
         Buku buku1 = new Buku("Matahari", "Aziz", "ISBN001", "Romance", true);
         Buku buku2 = new Buku("Bulan", "Mulhadi", "ISBN002", "Fantasy", true);
-        Notifikasi n1 = new Notifikasi(tp1);
         
-        
-        admin.addAnggota(ap1);
-        admin.addAnggota(ap2);
-        admin.addBuku(buku1);
-        admin.addBuku(buku2);
-        admin.setTransaksi_peminjaman(tp1);
-        admin.setTransaksi_pengembalian(tpengem1);
-
         ArrayList<AnggotaPerpustakaan> ap = admin.getAnggota();
         ArrayList<Buku> bk = admin.getDaftarBuku();
-//        System.out.println("Nama-nama anggota perpustakaan:");
-//        for(AnggotaPerpustakaan i : ap){
-//            System.out.println("ID Anggota  : " + i.getId_anggota());
-//            System.out.println("Nama Anggota  : " + i.getNama());
-//            System.out.println("No. Telpon  : " + i.getNo_telpon());
-//            System.out.println("-----------------------");
-//        }
-//        System.out.println("\nBuku-buku yang tersedia:");
-//        for(Buku j : bk){
-//            System.out.println("No. ISBN  : " + j.getNo_isbn());
-//            System.out.println("Judul Buku   : " + j.getJudul());
-//            System.out.println("Genre Buku  : " + j.getGenre());
-//            System.out.println("Nama Pengarang  : " + j.getPengarang());
-//            System.out.println("-----------------------");
-//        }
+        ArrayList<TransaksiPeminjaman> tpin = admin.getTransaksi_peminjaman();
+        admin.addAnggota(ap1); // Metode untuk menambahkan data anggota ke dalam sistem
+        admin.addAnggota(ap2); // Metode untuk menambahkan data anggota ke dalam sistem
+        admin.addBuku(buku1); // Metode untuk menambahkan data buku ke dalam sistem 
+        admin.addBuku(buku2); // Metode untuk menambahkan data buku ke dalam sistem
+        admin.displayAnggota(); // Metode untuk menampilkan data anggota yang tersimpan
+        admin.displayBuku(); // Metode untuk menampilkan data buku yang tersimpan
+        admin.setTransaksi_peminjaman(tpin1); // Metode untuk membuat transaksi peminjaman 
+        admin.setTransaksi_pengembalian(tkem1); // Metode untuk membuat transaksi pengembalian
+        System.out.println();
+        admin.displayAktivitasAnggota(ap1); // Metode untuk menampilkan aktivitas yang dilakukan oleh anggota
         
+        System.out.println();
         // Metode searchBuku
         ap.get(0).searchBuku("Matahari", admin);
-        System.out.println("---------------------\n");
-        
+        System.out.println("================================================\n");
         // Metode pinjamBuku
-        ap.get(0).pinjamBuku(bk.get(0), tp1);
-        System.out.println("Ketersediaan buku judul matahari: " + bk.get(0).isIsAvailable());
-        System.out.println("---------------------\n");
-        
+        ap.get(0).pinjamBuku(bk.get(0), tpin1);
+        System.out.println("Ketersediaan buku judul " + bk.get(0).getJudul() + " : " + bk.get(0).isIsAvailable());
+        System.out.println("================================================\n");
         // Metode restoreBuku
-        ap.get(0).restoreBuku(bk.get(0), tpengem1);
-        System.out.println("Ketersediaan buku judul matahari: " + bk.get(0).isIsAvailable());
+        ap.get(0).restoreBuku(bk.get(0), tkem1);
+        System.out.println("Ketersediaan buku judul " + bk.get(0).getJudul() + " : " + bk.get(0).isIsAvailable());
+        System.out.println("================================================\n");
         
-
-        
-        
+        ap.get(0).pinjamBuku(bk.get(0), tpin1);
+        System.out.println();
+        Notifikasi n1 = new Notifikasi(tpin.get(0), ap.get(0), bk.get(0));
+        n1.calculateDuration();
+        n1.pemberitahuan();
     }
-    
 }
